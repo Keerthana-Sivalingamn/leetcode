5096e6dff68dbb432aa07f6e9fb1964d44d7ecee
Keerthana S <bhanukeerthi2002@gmail.com>
2025-01-13 18:10:16 +0530

    Time: 24 ms (14.52%) | Memory: 22.4 MB (67.78%) - LeetSync

--- 2190-count-common-words-with-one-occurrence/count-common-words-with-one-occurrence.cpp
+++ 2190-count-common-words-with-one-occurrence/count-common-words-with-one-occurrence.cpp
@@ -4,11 +4,11 @@ public:
         map<string, int> mp;
         for(auto str:words1){
             if(mp[str]==0) mp[str]=1;
-            else if(mp[str]==1) mp[str]=-1; // condition failed
+            else if(mp[str]==1) mp[str]=-1; 
         }
         for(auto str:words2){
             if(mp[str]==1) mp[str]=2;
-            else if(mp[str]==2) mp[str]=-1; // more than one occurence, condition failed
+            else if(mp[str]==2) mp[str]=-1; 
         }
         int count=0;
         for(auto x:mp){
