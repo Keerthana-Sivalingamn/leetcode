f15048775664c3bf707001a6049d68510829ba7c
Keerthana S <bhanukeerthi2002@gmail.com>
2025-01-20 19:51:33 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (53.04%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -1,12 +1,23 @@
 class Solution {
 public:
-    string removeOuterParentheses(string S) {
-        string res;
-        int opened = 0;
-        for (char c : S) {
-            if (c == '(' && opened++ > 0) res += c;
-            if (c == ')' && opened-- > 1) res += c;
+    string removeOuterParentheses(string s) {
+        // optimal - using depth(virtual stack types) - O(n) , O(1)
+        string result; // answer without outermost parantheses
+        int depth=0;   // helps segregate primitive decomposition
+        for(auto ch: s)
+        {
+            if(ch=='(') // opening paranthesis
+            {
+                if(depth > 0) result += ch; //only add to result if it isn't outermost
+                depth++;      //signify going inside 
+            }
+
+            else if(ch==')')    //closing paranthesis
+            {
+                depth--;    //first decrement the depth counter
+                if(depth > 0) result += ch; //add to result only if not outermost paranthesis
+            }
         }
-        return res;
+        return result;
     }
 };
