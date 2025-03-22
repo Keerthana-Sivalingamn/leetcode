f75831315b8ec9185cec7542be7a4e8111b661bd
Keerthana S <bhanukeerthi2002@gmail.com>
2025-03-22 20:32:32 +0530

    Time: 27 ms (96.05%) | Memory: 58.2 MB (10.73%) - LeetSync

---
+++ 2655-rearrange-array-to-maximize-prefix-score/rearrange-array-to-maximize-prefix-score.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxScore(int[] nums) {
+        Arrays.sort(nums);
+        int t;
+        int i,c=0;
+        int l1=nums.length;
+        int[] l=new int[l1];
+    
+        for(i=0;i<l1/2;i++)
+        {
+            t=nums[i];
+            nums[i]=nums[l1-1-i];
+            nums[l1-1-i]=t;
+        }
+        long s=0;
+        for(i=0;i<l1;i++)
+        {
+            s=s+(long)nums[i];
+            if(s>0)
+              c++;
+            else
+              s=0;
+        }
+        return c;
+        
+    }
+}
